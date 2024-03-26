1fdaf4777ba263d37418647d6dab975c29d774b3
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-03-26 12:48:41 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20319884
 api_hash = "637e3ba6357aa3ba2f3bf5742e0fd066"
-bot_token = "6777421711:AAG1G_gmpMOyN7XdIY3i-_tAZmRN9US0EN8"
+bot_token = "6777421711:AAEK-TLyvrg7q-OzS-X-j4UT3j89V9TAYB0"
 
